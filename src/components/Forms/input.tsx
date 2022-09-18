3abe9e829464081d5093781676948c2cb29039b7
Fernando Bonfim <devnoob@vivaldi.net>
2022-09-18 07:10:23 -0300

    ProPlayer

---
+++ src/components/Forms/input.tsx
@@ -0,0 +1,13 @@
+import { InputHTMLAttributes } from 'react'
+
+// Props will have any number of input attributes
+interface Props extends InputHTMLAttributes<HTMLInputElement> {}
+
+export function Input(props: Props){
+  return(
+    <input
+      {...props}
+      className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500"
+    />
+  )
+}
