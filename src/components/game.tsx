9bd37c065e1548783184fcd76158c03549d578f7
Fernando Bonfim <devnoob@vivaldi.net>
2023-04-04 13:21:27 -0300

    Fixed game link

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -6,7 +6,7 @@ interface GameDetails{
 
 export default function Game(props : GameDetails){
   return(
-    <a href="" className="relative rounded-lg overflow-hidden">
+    <a href="#" className="relative rounded-lg overflow-hidden">
       <img src={props.thumbnail} />
       <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
         <strong className="font-bold text-white block">{props.name}</strong>
